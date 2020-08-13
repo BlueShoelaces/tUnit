975f7d4169dcc7e8c4dd3d1c450d19204a6d355e
BlueShoelaces <narruda789@gmail.com>
2020-08-13 15:13:39 -0400

    #14: Cleaning up console logs and imports.

--- test/tUnit/macros/assert-equals.js
+++ test/tUnit/macros/assert-equals.js
@@ -1,12 +1,7 @@
 Macro.add("assert-equals", {
     handler : function () {
         let expected = this.args[0];
-        // var actual = this.args[1];
         let actual = $("<div/>").wiki(this.args[1]).html();
-
-        
-        console.log("this.args[0] = " + expected);
-        console.log("this.args[1] = " + actual);
 
         if (expected == actual) {
             State.variables.tempOutput = "Passed!";
